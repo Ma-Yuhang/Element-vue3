fb2ded930bd007aa30b0172d98f5b76948fdbee7
189690 <2312165827@qq.com>
2024-01-18 09:32:22 +0800

    Update build configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,33 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import inspectPlugin from 'vite-plugin-inspect'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueJsx(),
-  ],
+  plugins: [vue(), vueJsx(), inspectPlugin()],
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vue: ['vue'],
+          'fontawesome-svg-core': ['@fortawesome/fontawesome-svg-core'],
+          'free-solid-svg-icons': ['@fortawesome/free-solid-svg-icons'],
+          'vue-fontawesome': ['@fortawesome/vue-fontawesome']
+        }
+        // manualChunks(id) {
+        //   if (id.includes('node_modules')) {
+        //     return 'vendor';
+        //   }
+        // }
+      }
+    }
+  }
 })
