0712653ae98386f3320a3f9f79a8893e15e27025
mayuhang <2312165827@qq.com>
2024-01-18 21:16:56 +0800

    fix: 修改.eslintrc.js文件名

--- src/router/routes.js
+++ src/router/routes.js
@@ -8,41 +8,41 @@ export default [
   {
     path: '/button',
     name: 'button',
-    component: () => import('../views/ButtonView.vue')
+    component: () => import('@/views/ButtonView.vue')
   },
   {
     path: '/card',
     name: 'about',
-    component: () => import('../views/CardView.vue')
+    component: () => import('@/views/CardView.vue')
   }
   ,
   {
     path: '/dialog',
     name: 'dialog',
-    component: () => import('../views/DialogView.vue')
+    component: () => import('@/views/DialogView.vue')
   }
   ,
   {
     path: '/pager',
     name: 'pager',
-    component: () => import('../views/PagerView.vue')
+    component: () => import('@/views/PagerView.vue')
   }
   ,
   {
     path: '/collapse',
     name: 'collapse',
-    component: () => import('../views/CollapseView.vue')
+    component: () => import('@/views/CollapseView.vue')
   }
   ,
   {
     path: '/tooltip',
     name: 'tooltip',
-    component: () => import('../views/TooltipView.vue')
+    component: () => import('@/views/TooltipView.vue')
   }
   ,
   {
     path: '/dropdown',
     name: 'dropdown',
-    component: () => import('../views/DropdownView.vue')
+    component: () => import('@/views/DropdownView.vue')
   }
 ]
