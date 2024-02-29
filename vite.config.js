b0493340daf743a90e4121afed866ce11b57a110
189690 <2312165827@qq.com>
2024-02-29 15:14:05 +0800

    feat: 优化环境变量

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,41 @@
 import { fileURLToPath, URL } from 'node:url'
 import inspectPlugin from 'vite-plugin-inspect'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
 
-// console.log('NODE_ENV', process.env.NODE_ENV);
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue(), vueJsx(), inspectPlugin()],
-  resolve: {
-    extensions: ['.js', '.vue', '.json'],
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vue: ['vue'],
-          'fontawesome-svg-core': ['@fortawesome/fontawesome-svg-core'],
-          'free-solid-svg-icons': ['@fortawesome/free-solid-svg-icons'],
-          'vue-fontawesome': ['@fortawesome/vue-fontawesome']
+export default defineConfig(({ command, mode }) => {
+  // 根据当前工作目录中的 `mode` 加载 .env 文件
+  // 设置第三个参数为 '' 来加载所有环境变量，而不管是否有 `VITE_` 前缀。
+  const env = loadEnv(mode, process.cwd(), '')
+
+  console.log('command', command)
+  console.log('mode', mode)
+  console.log('env', env.BASE_API_URL)
+  return {
+    plugins: [vue(), vueJsx(), inspectPlugin()],
+    resolve: {
+      extensions: ['.js', '.vue', '.json'],
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vue: ['vue'],
+            'fontawesome-svg-core': ['@fortawesome/fontawesome-svg-core'],
+            'free-solid-svg-icons': ['@fortawesome/free-solid-svg-icons'],
+            'vue-fontawesome': ['@fortawesome/vue-fontawesome']
+          }
+          // manualChunks(id) {
+          //   if (id.includes('node_modules')) {
+          //     return 'vendor';
+          //   }
+          // }
         }
-        // manualChunks(id) {
-        //   if (id.includes('node_modules')) {
-        //     return 'vendor';
-        //   }
-        // }
       }
     }
   }
